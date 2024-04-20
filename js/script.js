136bd0ddfee3ce6f24e2e35569034635d389993c
tongtonglove <3263302669@qq.com>
2024-04-20 22:53:23 +0800

    Site updated: 2024-04-20 22:53:23

--- js/script.js
+++ js/script.js
@@ -108,28 +108,6 @@
   }
 
   // Mobile nav
-  firework({
-    excludeElements: ["a"],
-    particles: [
-      {
-        shape: "circle",
-        move: "emit",
-        easing: "easeOutExpo",
-        colors: [
-          "rgba(255,182,185,.9)",
-          "rgba(250,227,217,.9)",
-          "rgba(187,222,214,.9)",
-          "rgba(138,198,209,.9)",
-        ],
-        number: 30,
-        duration: [1200, 1800],
-        shapeOptions: {
-          radius: [16, 32],
-        },
-      },
-    ]
-  });
-  
   var $container = $('#container'),
     isMobileNavAnim = false,
     mobileNavAnimDuration = 200;
